7616b08e66e5af8452ebc7e7912d15332884a89f
Ignacio Gavilanes <77927772+IgnacioGavilanes@users.noreply.github.com>
2022-03-23 10:10:28 -0300

    About component
    Still need to make visual improvements (specially on mobile)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,14 @@
 import './App.scss';
 import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from  './components/Hero/Hero';
-
+import {About} from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   );
 }
---
+++ portfolio/src/components/About/About.js
@@ -0,0 +1,45 @@
+import './About.scss';
+import React from 'react'
+
+export const About = () => {
+  return (
+    <section id='about'>
+         <div className='aboutTitleWrapper'>
+            <h2 className='aboutTitle'>
+                About.
+            </h2>
+        </div>
+
+        <div className='aboutWrapper'>
+            <p className='aboutInfo1'>It is kind of hard to tell you more about me without taking too much 
+                of your time so, here, these are some random things I like:
+            </p>
+
+            <div className='hobbiesWrapper'>
+                <ul className='hobbiesCol1'>
+                    <li>Videogames 🎮️</li>
+                    <li>Working out 💪🏼</li>
+                    <li>Coffee ☕</li>
+                    <li>Football ⚽️ </li>
+                    <li>Buying clothes 👞🤦🏻</li>
+                </ul>
+                <ul className='hobbiesCol2'>
+                    <li>Riding my bike 🚴🏼‍♂️</li>
+                    <li>Board games 🎲</li>
+                    <li>Making playlists 🎧️ </li>
+                    <li>Movies🍿 </li>
+                    <li>Traveling ✈</li>
+                </ul>
+            </div>
+
+            <p className='aboutInfo2'>After graduating highschool in Uruguay 🇺🇾 (my homecountry), I took off to the USA 🇺🇸 
+                where I decided to go to college and major in biochemistry 🧑🏻‍🔬‍🔬 Half way through my career I started programming 
+                as a hobbie 👨🏻‍💻 <br/><br/>
+                After graduating and getting back home, I got a 6-month research contract job in neuroscience. After these 6 months
+                 were up, I decided I wanted to pursue a career in development, so I started to take courses to level up...
+            </p>
+            
+        </div>
+    </section>
+  )
+}
